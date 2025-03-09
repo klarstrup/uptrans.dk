d001613c08412f203a4344d6455b16d5c32c08b2
Io Klarstrup <io@klarstrup.dk>
2025-03-09 18:41:45 +0100

    Remove dark mode inversion from image component

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,6 @@ export default async function Home() {
         </ul>
       </div>
       <Image
-        className="dark:invert"
         src={UpTransShonkTransPng}
         alt="Next.js logo"
         width={180}
