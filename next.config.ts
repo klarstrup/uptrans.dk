459f0d7127c0de059c90d348950526c1f6ee5f61
Io Klarstrup <io@klarstrup.dk>
2025-03-09 18:58:43 +0100

    Add experimental support for external server components with zlib-sync

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["zlib-sync"],
+  },
 };
 
 export default nextConfig;
